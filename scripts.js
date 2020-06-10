532c5a1469bc79eceac3e9cdbdbd2e2f74338e91
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-06-10 16:05:18 -0700

    add function to translate to pig latin
    Co-authored-by: Christine Augustine <blacchoy@gmail.com>

--- scripts.js
+++ scripts.js
@@ -1,8 +1,17 @@
+function pigLatin(array) {
+  if (array[0] === "a" || array[0] === "e" || array[0] === "i" || array[0] === "o" || array[0] === "u") {
+    return array.concat('a','y');
+  } else if (array[0] + array[1] === "qu") {
+    return array.slice(2).concat("q", "u", "a", "y");
+  } 
+}
 
-
+function convertToArray(string) {
+  return string.split("");
+}
 
 function handleErrors(sentence) {
-  const regex = /^[A-Za-z]+$/
+  const regex = /^[A-Za-z ]+$/
   const isValid = regex.test(sentence);
   if (!isValid) {
       alert("Please only enter letters!");
@@ -15,8 +24,11 @@ $( document ).ready(function() {
     event.preventDefault();
     const formInput = $("#userPhrase").val();
     const noErrors = handleErrors(formInput);   
-    
-    console.log(noErrors);
+    const inputArrayValue = convertToArray(noErrors);
+    const pigLatinTranslated = pigLatin(inputArrayValue);
+
+
+    console.log(pigLatinTranslated);
   });
 });
 
