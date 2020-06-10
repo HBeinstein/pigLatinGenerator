e05381c6f7566b5305dc72e6a0600c362ba6af97
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-06-10 15:36:24 -0700

    add .toLowerCase method to handleErrors function
    Co-authored-by: Christine Augustine <blacchoy@gmail.com>

--- scripts.js
+++ scripts.js
@@ -1,30 +1,22 @@
+
+
+
 function handleErrors(sentence) {
   const regex = /^[A-Za-z]+$/
   const isValid = regex.test(sentence);
   if (!isValid) {
-      alert("Only Alphabet allowed.");
+      alert("Please only enter letters!");
   }
-  return isValid;
+  return sentence.toLowerCase();
 }
-
-// function handleErrors(sentence) {
-//   if (sentence.includes('#') || sentence.includes("!")) {
-//     console.log("Please enter a valid word!");
-//   } 
-// }
-
 
 $( document ).ready(function() {
   $("form").submit(function() {
     event.preventDefault();
     const formInput = $("#userPhrase").val();
-
-    handleErrors(formInput);
-
-
-
-
+    const noErrors = handleErrors(formInput);   
+    
+    console.log(noErrors);
   });
 });
 
-// .search(/[\|&;\$%@"?'-<>\(\)\+,/]/g,"")
